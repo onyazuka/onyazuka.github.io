043fc7c850926f75cb7b0ca326e69b41bb84408f
onyazuka <onizuka2008@yandex.ru>
2019-10-18 11:45:41 +0300

    change

---
+++ react-haniwa-menu/build/precache-manifest.ee2664064b3f95e3db76c21106b0d79a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53aedf31d3c190447445406a57a0df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41e4d8538fc45727361",
+    "url": "/static/css/main.488a738c.chunk.css"
+  },
+  {
+    "revision": "5578eed3a2de5f219f07",
+    "url": "/static/js/2.f2992475.chunk.js"
+  },
+  {
+    "revision": "b41e4d8538fc45727361",
+    "url": "/static/js/main.3e6009a7.chunk.js"
+  },
+  {
+    "revision": "fe11e01e7f9ca669b25a",
+    "url": "/static/js/runtime-main.38f5704e.js"
+  }
+]);
